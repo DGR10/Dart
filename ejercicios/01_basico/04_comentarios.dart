276ef00883479f5ca6258d45e68459a50304e40b
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2020-11-22 00:28:25 +0100

    Comentarios

---
+++ ejercicios/01_basico/04_comentarios.dart
@@ -0,0 +1,27 @@
+/**
+ * Es igual que el comentario de la funcion /// Pero en multilinea
+ * Este archivo tiene por objetivo explicar los comentarios en dart
+ * [creadoEn] : Hoy
+ */
+
+main() {
+  
+  // Esta es una variable temporal que quiero definir
+  int a = 10; // a debería de ser 10
+
+  /* Comentarios en bloques
+  final personas = [
+    // Esta es la primera persona
+    'Carlos', // [0]
+    // Esta es la segunda persona
+    'Pedro', //[1]
+    // Esta es la tercera persona
+    'Fernando' // [2]
+  ];
+  */
+}
+
+// Para comentar con documentacion
+/// Funcion para saludar
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar( int nombre, int mensaje) {}
