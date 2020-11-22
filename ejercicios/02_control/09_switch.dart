9fbe7c93f8adb24172acf20e27f99c37afd6d20f
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2020-11-22 10:40:09 +0100

    switch ejemplos

--- ejercicios/02_control/09_switch.dart
+++ ejercicios/02_control/09_switch.dart
@@ -5,7 +5,7 @@ main() {
   int rnd = Random().nextInt(10);
   print(rnd);
 
-  switch (rnd) {
+  switch (rnd) { // 0,1,2,3,4,5,6,7,8,9   'carlos', 'angel'
     case 0:
       print('Lunes');
       break;
