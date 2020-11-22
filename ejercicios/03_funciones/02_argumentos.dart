9325f4b891c3cd25dab03126563626daafb668b1
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2020-11-22 16:07:14 +0100

    Funciones con argumentos

---
+++ ejercicios/03_funciones/02_argumentos.dart
@@ -0,0 +1,20 @@
+
+// Argumentos posicionales
+main(List<String> args) {
+  // saludar( 'Hola', 'Fernando' );
+  saludar2( veces: 10, mensaje: 'Hola', nombre: 'Fernando' );
+}
+
+// Argumentos Opcionales [argumentos,]
+void saludar( String mensaje, [ String nombre = '<insertar nombre>' ]  ) {
+  print('$mensaje $nombre' );
+}
+
+// Argumentos con nombre {argumentos,}
+void saludar2({ String nombre, String mensaje, int veces }) {
+
+  for (var i = 0; i < veces; i++) {
+    print('$mensaje $nombre');
+  }
+
+}
