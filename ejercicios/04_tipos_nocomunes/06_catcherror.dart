652ab37da23c87fcf69e1d4828d435f2e1d80632
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2020-11-23 00:32:11 +0100

    catch error

---
+++ ejercicios/04_tipos_nocomunes/06_catcherror.dart
@@ -0,0 +1,19 @@
+
+main() {
+
+  Future<String> timeout = Future.delayed(Duration(seconds: 3), () {
+
+    if( 1 == 1 ) {
+      throw 'Auxilio!, explotó esta cosa';
+    }
+
+    return 'Retorno del Future';
+  });
+
+  // Es importante que todos nuestros Futures tengan el manejo de una excepción
+  timeout.then(print)
+         .catchError((error) => print(error));
+
+  print('Fin del main');
+
+}
