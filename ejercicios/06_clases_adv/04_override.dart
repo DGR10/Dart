a3f5bc137d12bdb4278fcceb938dd2a78dd7193d
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2020-11-24 22:47:42 +0100

    override

---
+++ ejercicios/06_clases_adv/04_override.dart
@@ -0,0 +1,35 @@
+
+class Persona {
+  String nombre;
+  int edad;
+
+  Persona (this.nombre, this.edad);
+
+  void imprimirNombre() => print('Nombre: $nombre, Edad: $edad');
+
+}
+
+class Cliente extends Persona {
+
+  String direccion;
+  List ordenes = [];
+
+  Cliente (int edadActual, String nombreActual) : 
+    super(nombreActual, edadActual);
+
+  @override
+  void imprimirNombre() {
+    super.imprimirNombre();
+    print('Cliente: $nombre, Edad: $edad');
+  } 
+
+}
+
+
+main() {
+  
+  final carlos = new Cliente(26, 'Carlos');
+
+  carlos.imprimirNombre();
+
+}
