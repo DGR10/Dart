a1751d7d355d9899e7432d9c06cae02b56069e89
Carlos Guijarro <carlosguijarromartin@gmail.com>
2020-11-24 09:19:05 +0100

    constructores

--- ejercicios/05_clases/01_clase.dart
+++ ejercicios/05_clases/01_clase.dart
@@ -3,16 +3,19 @@ import 'clases/persona.dart';
 
 main() {
 
-  final persona = new Persona();
+  final persona = new Persona(edad: 26, nombre:'Carlos');
   persona..nombre = 'Carlos'
          ..edad = 26;
         //  ..bio = 'Nació en 1994'; // privado
+
+  final persona2 = new Persona.persona30('Lucía');
 
   // persona.edad = 26;
   // persona.bio = 'Nació en 1994';
 
   // print(persona.toString());
   print(persona);
+  print(persona2);
   persona.bio = 'Cambié el valor';
   print(persona.bio);
 
--- ejercicios/05_clases/clases/persona.dart
+++ ejercicios/05_clases/clases/persona.dart
@@ -15,9 +15,23 @@ class Persona {
   // }
 
   String get bio => _bio.toUpperCase(); // Tabién puedo hacerlo con funciones de flecha
-  set bio(String texto) => _bio = texto;
+  set bio(String texto) => this._bio = texto;
 
   // Constructores
+  // Persona() {
+  //   this.edad = edad;
+  //   this.nombre = nombre;
+  // }
+  Persona({ this.edad = 0, this.nombre = 'Sin nombre'});
+
+  Persona.persona30( this.nombre ) {
+    this.edad = 30;
+  }
+
+  Persona.persona40( String nombre ) {
+    this.nombre = nombre;
+    this.edad = 40;
+  }
 
   // Métodos
   @override
