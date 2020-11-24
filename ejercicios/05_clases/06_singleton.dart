082d050d9d5aad931c5ab55c5101ebbd37b9a1f1
Carlos Guijarro Martín <carlosguijarromartin@gmail.com>
2020-11-24 22:28:06 +0100

    singleton

---
+++ ejercicios/05_clases/06_singleton.dart
@@ -0,0 +1,20 @@
+
+
+
+import 'clases/mi_servicio.dart';
+
+main() {
+  
+  final spotifyService = new MiServicio();
+  spotifyService.url = 'https://api.spotify.com';
+
+  final spotifyService2 = new MiServicio();
+  spotifyService2.url = 'https://api.spotify.com/v2';
+
+  // Apuntan a un mismo lugar en memoria
+  print( spotifyService == spotifyService2); // True
+
+  print( spotifyService.url);
+  print( spotifyService2.url);
+
+}
