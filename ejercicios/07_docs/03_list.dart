53b27ec1876cb2b878d74c22649104c1e2fbdd9d
Carlos Guijarro <carlosguijarromartin@gmail.com>
2020-11-26 18:11:39 +0100

    part1 listas

---
+++ ejercicios/07_docs/03_list.dart
@@ -0,0 +1,42 @@
+
+
+import 'dart:_internal';
+
+main(List<String> args) {
+  List<int> lista = [1,2,3,4,5];
+  List<int> lista2 = [3,1,2,15,-10];
+  List<String> lista3 = ['Tony','Peter'];
+
+  print('Length: ${lista.length}');
+  // print('First: ${lista[0]}');
+  print('First: ${lista.first}');
+  print('Last: ${lista.last}');
+
+  print('is empty: ${lista.isEmpty}');
+  print('is not empty: ${lista.isNotEmpty}');
+
+  print('asMap: ${lista.asMap()}'); // También puedo utilizar su clave para sacar el valor lista.asMap()[2] -> 3
+  Map listaMapa = lista3.asMap();
+  print('listaMapa: ${listaMapa}');
+  print('listaMapa: ${listaMapa[1]}');
+
+  print('indexOf: ${ lista3.indexOf('Peter')}'); // -1 No lo encontró
+  
+  // int mayor3 = lista.indexWhere((numero) {
+  //   if ( numero > 3) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   } 
+  // });
+
+  int mayor3 = lista.indexWhere((numero) => numero > 3 ? true : false);
+  print('indexWhere mayor3: $mayor3');
+
+  print('Remove: ${lista3.remove('Tony')}');
+  print('Remove result: ${lista3}');
+
+  print('Shuffle: $lista.shuffle()'); // Recoloca la lista aleatoriamente
+  print('Sort: ${lista3.sort()}');
+  print('Reverse: ${lista2.reversed()}');
+}
